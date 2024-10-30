aa755bce568fbf7a1339a48406fb203c73a44105
davidpaz06 <davidalbertopaz@gmail.com>
2024-10-29 23:49:10 -0400

    first commit

---
+++ src/config/dbConfig.js
@@ -0,0 +1,13 @@
+export const dbConfig = {
+  user: "postgres",
+  host: "localhost",
+  password: "davidpaz06",
+  database: "test",
+  port: 5432,
+  ssl: false,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  maxUses: 7500,
+};
+  
