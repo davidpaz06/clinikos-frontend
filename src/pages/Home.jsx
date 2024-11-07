6447f030cece0e7c6b7383ea2f1b6ab7893c6afc
davidpaz06 <davidalbertopaz@gmail.com>
2024-11-07 12:55:56 -0400

    create appointment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,40 @@ const Home = () => {
   const navigate = useNavigate();
   const { username } = location.state || {};
 
+  const newAppointment = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/toProcess", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          methodName: "createAppointment",
+          objectName: "Appointment",
+          params: {
+            date: "2023-12-01",
+            time: "10:00",
+            doctorId: 1,
+            patientId: 1,
+          },
+        }),
+        credentials: "include",
+      });
+
+      if (response.ok) {
+        alert("Logged out successfully");
+        navigate("/");
+      } else {
+        const error = await response.text();
+        console.error("Error logging out:", error);
+        alert("Error logging out");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Error logging out");
+    }
+  };
+
   const logout = async () => {
     try {
       const response = await fetch("http://localhost:3000/logout", {
@@ -34,6 +68,7 @@ const Home = () => {
   return (
     <div>
       <h1>Welcome home, {username}</h1>
+      <button onClick={newAppointment}>New Appointment</button>
       <button onClick={logout}>Log Out</button>
     </div>
   );
